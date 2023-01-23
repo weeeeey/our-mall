1416bf5d6925757c5fca1cc878dc54a513f588c2
weeeeey <qser155@naver.com>
2023-01-23 20:45:58 +0900

    #kakaoProvider 세팅

--- mall/pages/api/auth/[...nextauth].tsx
+++ mall/pages/api/auth/[...nextauth].tsx
@@ -3,6 +3,9 @@ import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
 
+import KakaoProvider from "next-auth/providers/kakao";
+import Adapters from "next-auth/adapters";
+
 export default NextAuth({
     providers: [
         CredentialsProvider({
@@ -37,8 +40,20 @@ export default NextAuth({
                 return exUser;
             },
         }),
+        KakaoProvider({
+            clientId: process.env.KAKAO_CLIENT_ID,
+            clientSecret: process.env.KAKAO_CLIENT_SECRET,
+        }),
     ],
+    adapter: Adapters.Prisma.Adaptter({ client }),
     callbacks: {
+        /** 
+        어떤 데이터를 넘겨주고 싶으면 jwt 토큰에 데이터를 유지하고 session에서 처리해주면 된다
+        1. providers 내부의 async authorize 에 인증을 실행
+        (로그인 인증 성공시)
+        2. callbacks의 jwt 함수가 실행
+        3. callbacks의 session 함수가 실행
+        */
         async jwt({ token }) {
             return token;
         },
@@ -56,7 +71,8 @@ export default NextAuth({
             return session;
         },
     },
-    pages: {
-        signIn: "/sign-in",
-    },
+    // pages: {
+    //     signIn: "/sign-in",
+    //     signOut: "/",
+    // },
 });
